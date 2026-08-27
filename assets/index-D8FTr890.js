3920b8025c64f2151cf4ef22d52d66e439e539ba
Иван Игнаткин <vanich7@mail.ru>
2026-08-27 18:34:12 +0300

    Updates

---
+++ assets/index-D8FTr890.js
@@ -0,0 +1,5 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/parsePdf-B_ObL2Su.js","assets/index-BpK8Giad.js","assets/index-QhLWdxpb.css","assets/parseDocx-B8zMXkrU.js"])))=>i.map(i=>d[i]);
+import{_ as o}from"./index-BpK8Giad.js";const a=new Set(["txt","md","markdown"]),s=t=>t.replace(/\r\n?/g,`
+`).replace(/[ \t]+$/gm,"").replace(/\n{3,}/g,`
+
+`).replace(/^\n+|\n+$/g,""),p=async t=>{const e=t.name.split(".").pop()?.toLowerCase()??"";if(t.type==="application/pdf"||e==="pdf"){const{parsePdf:r}=await o(async()=>{const{parsePdf:n}=await import("./parsePdf-B_ObL2Su.js");return{parsePdf:n}},__vite__mapDeps([0,1,2]));return r(t)}if(t.type==="application/vnd.openxmlformats-officedocument.wordprocessingml.document"||e==="docx"){const{parseDocx:r}=await o(async()=>{const{parseDocx:n}=await import("./parseDocx-B8zMXkrU.js");return{parseDocx:n}},__vite__mapDeps([3,1,2]));return r(t)}if(t.type.startsWith("text/")||a.has(e))return s(await t.text());throw new Error("Поддерживаются PDF, DOCX, TXT и MD.")},i=Object.freeze(Object.defineProperty({__proto__:null,parseResumeFile:p},Symbol.toStringTag,{value:"Module"}));export{i,s as n};
